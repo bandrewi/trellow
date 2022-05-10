bde0d10c913bd8b3d916cef9d7deec1166401eea
andrew bui <bandrewi98@gmail.com>
2022-05-10 15:30:44 -0700

    added cascade delete on boards for lists and on lists for cards

--- app/models/board.py
+++ app/models/board.py
@@ -7,13 +7,14 @@ class Board(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', nullable=False))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
     user = db.relationship('User', back_populates='boards')
-    lists = db.relationship("List", back_populates='boards')
+    lists = db.relationship(
+        "List", back_populates='boards', cascade="all, delete")
 
     def to_dict(self):
         return {
--- app/models/list.py
+++ app/models/list.py
@@ -2,22 +2,23 @@ from .db import db
 from sqlalchemy.sql import func
 
 
-class Card(db.Model):
+class List(db.Model):
     __tablename__ = 'lists'
 
     id = db.Column(db.Integer, primary_key=True)
     order = db.Column(db.Integer, nullable=False)
     title = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', nullable=False))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     board_id = db.Column(db.Integer, db.ForeignKey(
-        'boards.id', nullable=False))
+        'boards.id'), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True),
                            server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
     user = db.relationship('User', back_populates='lists')
     board = db.relationship('Board', back_populates='lists')
-    cards = db.relationship("Card", back_populates='lists')
+    cards = db.relationship(
+        "Card", back_populates='lists', cascade="all, delete")
 
     def to_dict(self):
         return {
