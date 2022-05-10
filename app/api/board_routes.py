3d1e0e029cb2ff1a138c990615ba87aa2849a274
andrew bui <bandrewi98@gmail.com>
2022-05-10 16:19:29 -0700

    GET route for boards

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -5,7 +5,10 @@ from app.models import db, User, Board
 board_routes = Blueprint('boards', __name__)
 
 
-# @board_routes.route('/')
-# def boards():
-#     user = User.query.get(current_user.id)
-#     boards = {'boards': []}
+@board_routes.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+# GET
+def boards():
+    user = User.query.get(current_user.id)
+    # query data comes back in a list of class instances
+    boards = [board.to_dict() for board in user.boards]
+    return jsonify(boards)
