d0940f8e2c09f14a25a427bd0216d7a1a988952e
andrew bui <bandrewi98@gmail.com>
2022-05-10 21:36:58 -0700

    added order to list form

--- app/forms/list_form.py
+++ app/forms/list_form.py
@@ -1,8 +1,9 @@
 from tokenize import String
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
 
 class ListForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
+    order = IntegerField('order', validators=[DataRequired()])
