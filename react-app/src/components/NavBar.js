908b3d28e3e8a5d9cf4f26d134e0f95f79052e8b
andrew bui <bandrewi98@gmail.com>
2022-05-12 10:01:51 -0700

    navbar for user and nonuser

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,47 @@
-
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './navbar.css'
 
 const NavBar = () => {
+  const user = useSelector(state => state.session.user)
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
+    <div id='nav-bar'>
+      <ul id='nav-bar-ul'>
+        {user ? (
+          <>
+            <li>
+              <NavLink to='/' exact={true} activeClassName='active'>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </>) : (
+          <>
+            <li>
+              <NavLink to='/login' exact={true} activeClassName='active'>
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+        )
+        }
+        {/* <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        </li> */}
       </ul>
-    </nav>
+    </div>
   );
 }
 
