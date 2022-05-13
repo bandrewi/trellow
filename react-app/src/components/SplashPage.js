94ba0ae8b8aed105a435ee140d5744c48d0cb0e2
andrew bui <bandrewi98@gmail.com>
2022-05-13 16:24:08 -0700

    splash page css & html

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,10 +1,21 @@
+import './splash.css'
+
 export default function SplashPage() {
     return (
-        <div>
-            <h1>Trellow helps teams move work forward.</h1>
-            <p>
-                Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.
-            </p>
-        </div>
+        <section>
+            <div id='big-container-1'>
+                <div id='container-1'>
+                    <div id='container-1-text'>
+                        <h1 id='container-1-heading'>Trellow helps teams move work forward.</h1>
+                        <p id='container-1-paragraph'>
+                            Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.
+                        </p>
+                    </div>
+                    <div id='img-1-container'>
+                        <img id='img-1' src='https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/spirit/hero/6a3ccd8e5c9a0e8ebea4235d12da6b24/hero.png' alt='' />
+                    </div>
+                </div>
+            </div>
+        </section >
     )
 }
