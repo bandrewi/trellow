63b7356e7f9c7b266c53aac1778be11dbd142aba
andrew bui <bandrewi98@gmail.com>
2022-05-18 18:46:01 -0700

    redirects to splash page after logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -10,6 +10,7 @@ const LogoutButton = () => {
     // WANT TO REDIRECT TO LOGIN PAGE BUT WON"T WORK BECAUSE OF HOW APP IS SET UP
     // NAVBAR IS IN USER COMP SO CAN"T ACCESS LOGIN PAGE ONCE ROUTED TO IT
     // return <Redirect to='/login' />;
+    return <Redirect to='/' />;
   };
 
   return <button onClick={onLogout}>Logout</button>;
