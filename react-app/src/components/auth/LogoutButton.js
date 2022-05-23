fa731cfc35dae66447db3ac2e7614a44b40b92d3
andrew bui <bandrewi98@gmail.com>
2022-05-23 08:52:01 -0700

    dispatched clear store on logout & on logout redirects to splash page

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import clearStore from '../../store/clear';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
@@ -8,10 +9,10 @@ const LogoutButton = () => {
   const history = useHistory()
   const onLogout = async (e) => {
     await dispatch(logout());
-    // history.push('/')
+    await dispatch(clearStore())
+    history.push('/')
     // trying to logout when on a single board page messes up the login page css
-    // for some reason works on heroku
-    history.push('/login')
+    // history.push('/login')
   };
 
   return <button id='logout-btn' onClick={onLogout}>Log out</button>;
